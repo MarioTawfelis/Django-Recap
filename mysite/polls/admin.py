28fc12a746922857211a1a1e72411b599ea3d458
Mario <mario.tawfelis@outlook.com>
2024-01-19 12:38:21 +0200

    Completed Part 7

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -16,7 +16,6 @@ class QuestionAdmin(admin.ModelAdmin):
     list_display = ["question_text", "pub_date", "was_published_recently"]
     list_filter = ["pub_date"]
     search_fields = ["question_text"]
-    # admin.AdminSite.site_header = "Custom site header"
 
 
 
